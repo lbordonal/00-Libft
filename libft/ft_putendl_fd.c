661871b2b932243d744568a8ecdf72f93706cf23
Lucas Henrique Bordonal <lbordona@c1r9s1.42porto.com>
2022-11-08 16:11:51 +0000

    08/11/22 - 16:11

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,13 +6,25 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:33:44 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/07 16:11:02 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/08 15:52:47 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* void	ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
 
+/* int	main(void)
+{
+	char	a[] = "Lucas";
+
+	printf("%s", "a = ");
+	printf("%s\n", a);
+	printf("%s\n\n", "ft_putendl_fd =");
+	ft_putendl_fd(a, 1);
+	return (0);
 } */
--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:31:29 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/08 15:15:07 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/08 15:35:17 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int	i;
 
+	i = 0;
+	if (!s || !f)
+		return ;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
 }
