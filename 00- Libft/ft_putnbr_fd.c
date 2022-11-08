3ca0b4ba9d82b2e99939837e4523cbe2ecc7fadb
Lucas Bordonal <lbordonal@gmail.com>
2022-11-08 00:26:17 +0000

    08/11/22 - 00:26

--- 00- Libft/ft_putnbr_fd.c
+++ 00- Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:34:26 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/07 18:06:52 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/08 00:02:52 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else
+	{
+		if (n < 0)
 		{
 			ft_putchar_fd('-', fd);
-			ft_putchar_fd('2', fd);
-			ft_putnbr_fd(147483648, fd);
+			n = -n;
+		}
+		if (n > 9)
+		{
+			ft_putnbr_fd((n / 10), fd);
+			ft_putnbr_fd((n % 10), fd);
 		}
 		else
 		{
-			if (n < 0)
-			{
-				ft_putchar_fd('-', fd);
-				n = -n;
-			}
-			if (n > 9)
-			{
-				ft_putnbr_fd((n / 10), fd);
-				ft_putnbr_fd((n % 10), fd);
-			}
-			else
-			{
-				ft_putchar_fd((n + '0'), fd);
-			}
+			ft_putchar_fd((n + '0'), fd);
 		}
+	}
 }
 
 /* int	main(void)
