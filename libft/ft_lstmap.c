db33d6256aa0a2415ea11228591ef7b4f9b52339
Lucas Bordonal <lbordonal@gmail.com>
2022-11-09 16:09:48 +0000

    09/11/22 - 16:09

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 01:22:42 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/09 15:57:54 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/09 16:09:18 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_node;
 	t_list	*new_list;
+	t_list	*new_node;
 
-	if(!lst || !f || !del)
-		return ;
-	new_list = ft_lstnew(f(lst->content));
-	if (!new_list)
-		return ;
-	lst = lst->next;
+	if (!f || !del)
+		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		if (new_node == NULL)
 		{
 			ft_lstclear(&new_list, del);
-			return ;
+			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
